b799a7fba83f03f897bc40f3c405173b17df8a37
MO0nlitgrace <moonlitgrace.gaia@gmail.com>
2025-06-06 20:52:47 +0530

    feat: add gitloom icon and update favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
+import GitloomIcon from "@/components/icons/gitloom";
 
 export default function Home() {
   return (
-    <div className="p-5 flex items-center gap-5">
-      <h1 className="text-2xl font-black">Gitloom</h1>
-      <Button size={"sm"}>Log in</Button>
+    <div className="p-5 flex items-center gap-2">
+      <GitloomIcon className="size-6" />
+      <h1 className="text-xl font-black">Gitloom</h1>
     </div>
   );
 }
---
+++ src/components/icons/gitloom.tsx
@@ -0,0 +1,17 @@
+export default function GitloomIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      width="300"
+      height="300"
+      viewBox="0 0 300 300"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M300 150L150 300L0 150L150 0L300 150ZM74.9053 149.905L149.905 224.905L224.905 149.905H74.9053Z"
+        fill="#FAFAFA"
+      />
+    </svg>
+  );
+}
