bc59158c5a7f548a1fd37aff50bf5bb638bd48e5
MO0nlitgrace <moonlitgrace.gaia@gmail.com>
2025-06-06 20:46:04 +0530

    chore: remove devIndicators and test UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Gitloom</h1>
+    <div className="p-5 flex items-center gap-5">
+      <h1 className="text-2xl font-black">Gitloom</h1>
       <Button size={"sm"}>Log in</Button>
     </div>
   );
