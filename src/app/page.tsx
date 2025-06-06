b28dfb5b537747f7e0b9cb5efdbd807df7c5a796
MO0nlitgrace <moonlitgrace.gaia@gmail.com>
2025-06-06 20:57:31 +0530

    chore: configure prettier and format codebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import GitloomIcon from "@/components/icons/gitloom";
+import GitloomIcon from '@/components/icons/gitloom';
 
 export default function Home() {
   return (
-    <div className="p-5 flex items-center gap-2">
+    <div className="flex items-center gap-2 p-5">
       <GitloomIcon className="size-6" />
       <h1 className="text-xl font-black">Gitloom</h1>
     </div>
