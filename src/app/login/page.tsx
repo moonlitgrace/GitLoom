1fa4ce45883c87dc5a5679ae9ab096ccdd3c1a0c
MO0nlitgrace <moonlitgrace.gaia@gmail.com>
2025-06-07 20:13:04 +0530

    chore: add metadata to login route page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,12 @@
 import GithubIcon from '@/components/icons/github';
 import GitloomIcon from '@/components/icons/gitloom';
 import { Button } from '@/components/ui/button';
+import { Metadata } from 'next';
 import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: 'Login — Gitloom',
+};
 
 export default function Page() {
   return (
