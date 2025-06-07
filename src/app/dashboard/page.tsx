0c1d33e82e896ea09c27848fc3f05c50781cc7c2
MO0nlitgrace <moonlitgrace.gaia@gmail.com>
2025-06-07 17:13:00 +0530

    feat: add github like code explorer

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import { Avatar, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import { Input, InputIcon, InputRoot } from '@/components/ui/input';
-import { NavigationOff, Plus, Search } from 'lucide-react';
+import { Folder, NavigationOff, Plus, Search } from 'lucide-react';
 
 export default function Page() {
   return (
@@ -17,37 +17,56 @@ export default function Page() {
         </div>
       </div>
       <div className="col-span-2 flex flex-col gap-2">
-        <div className="flex items-center gap-2">
-          <Avatar className="size-5">
-            <AvatarImage src={'https://github.com/moonlitgrace.png'} />
-          </Avatar>
-          <Button variant={'ghost'} className="text-muted-foreground" asChild>
-            <a href="https://github.com/moonlitgrace" target="_blank" rel="noreferrer noopener">
-              moonlitgrace
-            </a>
-          </Button>
-          <span className="text-muted-foreground/50 text-xl">/</span>
-          <Button variant={'ghost'} className="font-bold" asChild>
+        <div className="flex items-center gap-4">
+          <div className="flex items-center gap-2">
+            <Avatar className="size-5">
+              <AvatarImage src={'https://github.com/moonlitgrace.png'} />
+            </Avatar>
+            <Button variant={'ghost'} className="text-muted-foreground" asChild>
+              <a href="https://github.com/moonlitgrace" target="_blank" rel="noreferrer noopener">
+                moonlitgrace
+              </a>
+            </Button>
+            <span className="text-muted-foreground/50 text-xl">/</span>
+            <Button variant={'ghost'} className="font-bold" asChild>
+              <a
+                href="https://github.com/moonlitgrace/gitloom-repo"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                gitloom-repo
+              </a>
+            </Button>
+          </div>
+          <div className="flex flex-1 items-center gap-4">
+            <InputRoot>
+              <InputIcon>
+                <Search />
+              </InputIcon>
+              <Input placeholder="Search..." disabled />
+            </InputRoot>
+            <Button size={'default'}>
+              <Plus />
+              Add New
+            </Button>
+          </div>
+        </div>
+        <div className="mt-2 divide-y overflow-hidden rounded-md border">
+          <div className="hover:bg-secondary/50 grid grid-cols-5 gap-2 p-3">
+            <div className="col-span-2 flex items-center gap-2">
+              <Folder className="fill-muted-foreground text-muted-foreground size-4" />
+              <button className="text-sm hover:underline">.gitloom</button>
+            </div>
             <a
-              href="https://github.com/moonlitgrace/gitloom-repo"
+              href="https://github.com/GitloomLabs/Gitloom/commit/ca72cffb748c9e9be0a5db83604ebbe81506946f"
               target="_blank"
               rel="noreferrer noopener"
+              className="text-muted-foreground col-span-2 text-sm hover:underline"
             >
-              gitloom-repo
+              chore: update .gitloom settings
             </a>
-          </Button>
-        </div>
-        <div className="flex items-center gap-4">
-          <InputRoot>
-            <InputIcon>
-              <Search />
-            </InputIcon>
-            <Input placeholder="Search..." />
-          </InputRoot>
-          <Button size={'default'}>
-            <Plus />
-            Add New
-          </Button>
+            <span className="text-muted-foreground ml-auto text-sm">2 hours ago</span>
+          </div>
         </div>
       </div>
     </div>
