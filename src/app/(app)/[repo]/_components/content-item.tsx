1c8ab7d2701c48fb724a9b612d77da56d116ece7
MO0nlitgrace <moonlitgrace.gaia@gmail.com>
2025-06-15 14:27:19 +0530

    feat: add content item and skeleton components

---
+++ src/app/(app)/[repo]/_components/content-item.tsx
@@ -0,0 +1,39 @@
+import datetime from '@/lib/date-time';
+import { cn } from '@/lib/utils';
+import { Content } from '@/types/github';
+import { File, Folder } from 'lucide-react';
+
+interface Props {
+  username: string | undefined;
+  repo: string;
+  content: Content;
+}
+
+export default function ContentItem({ username, repo, content }: Props) {
+  const Icon = content.type === 'dir' ? Folder : File;
+
+  return (
+    <div key={content.path} className="hover:bg-secondary/50 grid grid-cols-5 gap-2 p-3">
+      <div className="col-span-2 flex items-center gap-2">
+        <Icon
+          className={cn(
+            'text-muted-foreground size-4',
+            content.type === 'dir' && 'fill-muted-foreground',
+          )}
+        />
+        <button className="text-sm hover:underline">{content.name}</button>
+      </div>
+      <a
+        href={`https://github.com/${username}/${repo}/commit/${content.lastCommit.sha}`}
+        target="_blank"
+        rel="noreferrer noopener"
+        className="text-muted-foreground col-span-2 text-sm hover:underline"
+      >
+        {content.lastCommit.message}
+      </a>
+      <span className="text-muted-foreground ml-auto text-sm">
+        {datetime(content.lastCommit.date).fromNow()}
+      </span>
+    </div>
+  );
+}
