65ad199afca1ccf70783fe66241820be03b8a4e4
stabldev <114811070+stabldev@users.noreply.github.com>
2025-06-20 08:03:24 +0530

    refactor: move active field logics to provider (#58)
    * refactor: move active field logics to provider
    
    * chore: remove ghost component

--- src/app/(app)/~/[repo]/new/_providers/active-field.provider.tsx
+++ src/app/(app)/~/[repo]/new/_providers/active-field.provider.tsx
@@ -12,9 +12,34 @@ export function ActiveFieldProvider({ children }: ActiveFieldProviderProps) {
     { id: `text-${nanoid()}`, fieldId: 'text' },
   ]);
 
+  function deleteField(id: string) {
+    setActiveFields((prev) => prev.filter((f) => f.id !== id));
+  }
+
+  function addField({ fieldId, key, value }: Omit<ActiveField, 'id'>) {
+    setActiveFields((prev) => [
+      ...prev,
+      { id: `${fieldId}-${nanoid()}`, fieldId: fieldId, key, value },
+    ]);
+  }
+
+  function changeField(id: string, type: 'key' | 'value', value: string) {
+    setActiveFields((prev) => prev.map((f) => (f.id === id ? { ...f, [type]: value } : f)));
+  }
+
+  function resetField(id: string) {
+    setActiveFields((prev) =>
+      prev.map((f) => (f.id === id ? { ...f, key: undefined, value: undefined } : f)),
+    );
+  }
+
   const contextValue: ActiveFieldContextValue = {
     activeFields,
     setActiveFields,
+    deleteField,
+    addField,
+    changeField,
+    resetField,
   };
 
   return <ActiveFieldContext.Provider value={contextValue}>{children}</ActiveFieldContext.Provider>;
